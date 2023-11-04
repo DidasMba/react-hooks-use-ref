c06fd83941ec997e806b490457b5b012ca00d88d
DidasMba <daniel.mbarushi@gmail.com>
2023-11-04 23:49:11 +0300

    add Ticker

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,9 +1,47 @@
+/*import React, { useEffect, useState } from "react";
+import { makeRandomNumber } from "../utils";
+
+function Ticker() {
+  const [price, setPrice] = useState(0);
+  const [color, setColor] = useState("black");
+
+  useEffect(() => {
+    const id = setInterval(() => setPrice(makeRandomNumber), 1000);
+    return function () {
+      clearInterval(id);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>TickerMaster</h1>
+      <h2 style={{ color: color }}>Price: ${price}</h2>
+    </div>
+  );
+}
+
+export default Ticker;*/
+
+
 import React, { useEffect, useState } from "react";
 import { makeRandomNumber } from "../utils";
 
 function Ticker() {
   const [price, setPrice] = useState(0);
   const [color, setColor] = useState("black");
+  const prevPrice = useRef(price);
+
+  useEffect(() => {
+    const prevPriceValue = prevPrice.current;
+    if (price > prevPriceValue) {
+      setColor("green");
+    } else if (price < prevPriceValue) {
+      setColor("red");
+    } else {
+      setColor("black");
+    }
+    prevPrice.current = price;
+  }, [price]);
 
   useEffect(() => {
     const id = setInterval(() => setPrice(makeRandomNumber), 1000);
