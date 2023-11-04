af44e534a88442942ca6ab12117bca49f1d8e0cc
DidasMba <daniel.mbarushi@gmail.com>
2023-11-04 23:46:20 +0300

    add count

--- src/components/CounterRef.js
+++ src/components/CounterRef.js
@@ -1,3 +1,23 @@
+/*import React, { useRef } from "react";
+
+function CounterRef() {
+  const count = useRef(0);
+
+  function handleClick() {
+    count.current = count.current + 1;
+    console.log(count.current);
+  }
+
+  return (
+    <div>
+      <h1>CounterRef</h1>
+      <button onClick={handleClick}>{count.current}</button>
+    </div>
+  );
+}
+
+export default CounterRef;*/
+
 import React, { useRef } from "react";
 
 function CounterRef() {
@@ -17,3 +37,4 @@ function CounterRef() {
 }
 
 export default CounterRef;
+
